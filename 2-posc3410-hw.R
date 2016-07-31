89a8a4c220370dbbf341a57c9bd137d62faab76a
Steven V. Miller <steven.v.miller@gmail.com>
2016-07-31 13:23:48 -0400

    Create 2-posc3410-hw.R

---
+++ 2-posc3410-hw.R
@@ -0,0 +1,19 @@
+library(RCurl)
+
+data <- getURL("https://raw.githubusercontent.com/svmiller/wvs-usa-strong-leader/master/wvs-usa-sl.csv")
+Data <- read.csv(text = data)
+
+M1 <- glm(sldummy ~ z_age + I(z_age^2) + female +
+              hsedorless + z_ideo + z_incscale + gop + unemployed +
+              + z_lemanc + gop*hsedorless,
+            data=subset(Data), family=binomial(link = "logit"))
+
+M2 <- glm(sldummy ~ z_age + I(z_age^2) + female +
+            hsedorless + z_ideo + z_incscale + gop + unemployed +
+            + z_lemanc + gop*hsedorless,
+          data=subset(Data, year == 2011), family=binomial(link = "logit"))
+
+M3 <- glm(hddummy ~ z_age + I(z_age^2) + female +
+            hsedorless + z_ideo + z_incscale + gop + unemployed +
+            + z_lemanc + gop*hsedorless,
+          data=subset(Data), family=binomial(link = "logit"))
