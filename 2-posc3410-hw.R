94110ceaeab526328089ea70f3e8e15d14cd505c
Steven V. Miller <steven.v.miller@gmail.com>
2016-11-01 12:17:28 -0400

    Update 2-posc3410-hw.R

--- 2-posc3410-hw.R
+++ 2-posc3410-hw.R
@@ -8,12 +8,18 @@ M1 <- glm(sldummy ~ z_age + I(z_age^2) + female +
               + z_lemanc + gop*hsedorless,
             data=subset(Data), family=binomial(link = "logit"))
 
+summary(M1)
+
 M2 <- glm(sldummy ~ z_age + I(z_age^2) + female +
             hsedorless + z_ideo + z_incscale + gop + unemployed +
             + z_lemanc + gop*hsedorless,
           data=subset(Data, year == 2011), family=binomial(link = "logit"))
 
+summary(M2)
+
 M3 <- glm(hddummy ~ z_age + I(z_age^2) + female +
             hsedorless + z_ideo + z_incscale + gop + unemployed +
             + z_lemanc + gop*hsedorless,
           data=subset(Data), family=binomial(link = "logit"))
+
+summary(M3)
